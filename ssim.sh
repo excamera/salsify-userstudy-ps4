2c2f3fd9bd0af490aa9357ffa194ebbd9a0fdda1
jremmons <emmons.john@gmail.com>
2017-09-13 15:50:38 -0700

    fixed up the ssim script to not repeat computations if possible

--- ssim.sh
+++ ssim.sh
@@ -3,13 +3,12 @@
 for dir in $(ls -d */); do
     cd $dir
     echo $dir
-    rm -f ssim.log
     if [[ ! -f ssim.log ]]; then
 	    /video-drive1-local/jemmons/convert.sh beforeFile.raw beforeFile.y4m &
 	    /video-drive1-local/jemmons/convert.sh afterFile.raw afterFile.y4m &
 	    wait
 	    
-	    dump_ssim -p 48 beforeFile.y4m afterFile.y4m | tee ssim.log
+	    dump_ssim -p 8 beforeFile.y4m afterFile.y4m | tee ssim.log
     fi
     cd ..
 done
